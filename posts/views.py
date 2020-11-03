9c37156a296d22e195c348c2632cb469ed6d81e7
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-11-03 22:31:39 +0000

    posts now posting

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, PostView, Love, Comment
+from .forms import PostForm
 
 
 class PostListView(ListView):
@@ -12,25 +13,31 @@ class PostDetailView(DetailView):
 
 
 class PostCreateView(CreateView):
+    form_class = PostForm
     model = Post
-    fields = (
-        'title',
-        'content',
-        'thumbnail',
-        'author',
-        'slug',
-    )
+    success_url = '/'
 
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'view_type' : 'create'
+        })
+        return context
+    
+    
 class PostUpdateView(UpdateView):
+    form_class = PostForm
     model = Post
-    fields = (
-        'title',
-        'content',
-        'thumbnail',
-        'author',
-        'slug',
-    )
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'view_type' : 'update'
+        })
+        return context
+    
 
 
 class PostDeleteView(DeleteView):
